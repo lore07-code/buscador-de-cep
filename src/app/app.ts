651d81c8b1877b9dd3752df4f049ce80a63b6154
Lorenna Costa Dos Reis de Deus <nena.deus07@gmail.com>
2026-09-21 16:03:17 -0300

    inicial

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Formcep } from './features/formcep/formcep';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Formcep],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('buscador-de-cap');
+}
